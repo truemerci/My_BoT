ed1a71de10eae9feaabbc35edb79632a77b869e3
truemerci <danilgladkiy7@gmail.com>
2023-05-23 21:41:07 +0300

    Add function 'statistic'and finalizing the code

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 import os
 import logging
+import datetime
 
+from datetime import date
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler
-
 
 TOKEN = "5972075747:AAEF3eI76CBMHhE1Hj18pNxbH_vupKdjyO8"
 filename = "expense.txt"
 
 logging.basicConfig(
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        level=logging.INFO
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
 )
 
 list_categories = ["health", "food", "clothes", "other"]
 
 
 class Finance:
-    def __init__(self, number: int, category: str):
+    def __init__(self, number: int, category: str, date_time: date = None):
         self.number = number
         self.category = category
+        self.datetime = date_time
 
     def __str__(self):
         if self.number >= 0:
-            return f"Income: {self.number} | Category: {self.category}"
+            return f"Income: {self.number}, Category: {self.category}, Date: {self.datetime}"
         else:
-            return f"Expense: {self.number} | Category: {self.category}"
+            return f"Expense: {self.number}, Category: {self.category}, Date: {self.datetime}"
 
 
 async def start(update: Update, context: CallbackContext) -> None:
@@ -40,6 +42,8 @@ async def start(update: Update, context: CallbackContext) -> None:
         "List income: /list_incomes\n"
         "Remove expense: /remove_expenses <expense number>\n"
         "Remove income: /remove_incomes <income number>\n"
+        "Statistic expense: /statistic_expense <category>, <day, week, month, or year>\n"
+        "Statistic income: /statistic_income <category>, <day, week, month, or year>\n"
     )
 
 
@@ -62,14 +66,13 @@ async def add_expense_or_income(update: Update, context: CallbackContext) -> Non
         if category not in list_categories:
             await update.message.reply_text(f"No such {category} category")
             return
-
-        expenses = Finance(int(finances), category)
+        expenses = Finance(int(finances), category, date.today())
         with open('expense.txt', 'a') as f:
             f.write(f'{expenses}\n')
         await update.message.reply_text(f"Your add {expenses}")
 
     else:
-        expenses = Finance(int(finances), category)
+        expenses = Finance(int(finances), category, date.today())
         with open('income.txt', 'a') as f:
             f.write(f'{expenses}\n')
         await update.message.reply_text(f"Your add {expenses}")
@@ -117,6 +120,63 @@ async def remove(update: Update, context: CallbackContext, data_file: str) -> No
         f.truncate()
 
 
+async def statistic_expense(update: Update, context: CallbackContext) -> None:
+    await statistic(update, context, "expense.txt")
+
+
+async def statistic_income(update: Update, context: CallbackContext) -> None:
+    await statistic(update, context, "income.txt")
+
+
+async def statistic(update: Update, context: CallbackContext, data_file: str) -> None:
+    user_input = "".join(context.args).split(",")
+
+    category = user_input[0]
+    period = user_input[1]
+
+    if period not in ["day", "week", "month", "year"]:
+        await update.message.reply_text("The period specified is incorrect. Select: day, week, month or year")
+        return
+
+    total_expenses = 0
+
+    with open(data_file, 'r') as f:
+        lines = f.readlines()
+
+        today = datetime.date.today()
+
+        for line in lines:
+            fields = line.strip().split(',')
+            item_category = fields[1].split(': ')[1]
+            amount = int(fields[0].split(': ')[1])
+
+            if item_category != category:
+                continue
+
+            data = fields[2].split(': ')[1]
+            datetime_obj = datetime.datetime.strptime(data, "%Y-%m-%d").date()
+
+            if period == "day":
+                if datetime_obj == today:
+                    total_expenses += amount
+            elif period == "week":
+                start_of_week = today - datetime.timedelta(days=today.weekday())
+                end_of_week = start_of_week + datetime.timedelta(days=6)
+                if start_of_week <= datetime_obj <= end_of_week:
+                    total_expenses += amount
+            elif period == "month":
+                if datetime_obj.year == today.year and datetime_obj.month == today.month:
+                    total_expenses += amount
+            elif period == "year":
+                if datetime_obj.year == today.year:
+                    total_expenses += amount
+
+    if total_expenses == 0:
+        await update.message.reply_text(f"No expenses found for the category {category} during {period}")
+    else:
+        await update.message.reply_text(f"Total expenses for {period} and category {category}: {total_expenses}")
+
+
 def run():
     app = ApplicationBuilder().token(TOKEN).build()
     logging.info("Application build successfully!")
@@ -128,7 +188,8 @@ def run():
     app.add_handler(CommandHandler("list_incomes", list_income))
     app.add_handler(CommandHandler("remove_expenses", remove_expense))
     app.add_handler(CommandHandler("remove_incomes", remove_income))
-
+    app.add_handler(CommandHandler("statistic_expenses", statistic_expense))
+    app.add_handler(CommandHandler("statistic_incomes", statistic_income))
     app.run_polling()
 
 
